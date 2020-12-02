199a14afeeabfb2f54e8524bf9ccc11d4c483ffa
Maxwell <msf9542@g.rit.edu>
2020-12-02 12:18:51 -0500

    finished Part2

---
+++ Day_2/Part2.py
@@ -0,0 +1,34 @@
+with open('../Day_2/input.txt', 'r') as file:
+    data = [line.strip().split() for line in file]
+
+for i, element in enumerate(data):
+    data[i][1] = element[1][0]
+    data[i][0] = element[0].split('-')
+
+print(sum(((line[2][int(line[0][0]) - 1] == line[1]) ^ (
+    line[2][int(line[0][1]) - 1] == line[1]) for line in data)))
+
+valid = 0
+
+for line in data:
+    i, j = line[0]
+    i, j = int(i) - 1, int(j) - 1
+
+    password = line[2]
+
+    letter = line[1]
+
+    try:
+        i = password[i] == letter
+    except:
+        i = False
+
+    try:
+        j = password[j] == letter
+    except:
+        j = False
+
+    if i ^ j:
+        valid -= -1
+
+print(valid)
