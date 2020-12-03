f92dc7efb8d372dfe1183ccc003e4698d793d3fe
Maxwell <msf9542@g.rit.edu>
2020-12-02 23:03:16 -0500

    basic conversion to have proper types

---
+++ Day_2/Part1.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    FILE *file;
+    file = fopen("input.txt", "r");
+
+    //read file into array
+    char range[1000][6];
+    char letter[1000][4];
+    char password[1000][16];
+    int i;
+
+    if (file == NULL)
+    {
+        printf("Error Reading File\n");
+        exit(0);
+    }
+
+    for (i = 0; i < 1000; i++)
+    {
+        fscanf(file, "%s", range[i]);
+        fscanf(file, "%s", letter[i]);
+        fscanf(file, "%s", password[i]);
+    }
+
+    char low[1000][3];
+    char high[1000][3];
+    char temp[2];
+
+    for (i = 0; i < 1000; i++)
+    {
+        if (range[i][1] == '-')
+        {
+            low[i][0] = range[i][0];
+            if (range[i][3] == '\0')
+            {
+                high[i][0] = range[i][2];
+            }
+            else
+            {
+                high[i][0] = range[i][2];
+                high[i][1] = range[i][3];
+            }
+        }
+        else
+        {
+            low[i][0] = range[i][0];
+            low[i][1] = range[i][1];
+            if (range[i][4] == '\0')
+            {
+                high[i][0] = range[i][3];
+            }
+            else
+            {
+                high[i][0] = range[i][3];
+                high[i][1] = range[i][4];
+            }
+        }
+    }
+
+    int nums[1000][2];
+
+    for (i = 0; i < 1000; i++)
+    {
+        nums[i][0] = atoi(low[i]);
+        nums[i][1] = atoi(high[i]);
+    }
+    printf("%d %d\n", nums[0][0], nums[0][1]);
+}
