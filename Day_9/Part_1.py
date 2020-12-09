5167ddd52067b2f2344f96d8c97d75eff8479870
Maxwell <msf9542@g.rit.edu>
2020-12-09 00:16:12 -0500

    finished Part1

--- Day_9/Part_1.py
+++ Day_9/Part_1.py
@@ -1,2 +1,20 @@
 with open('Day_9/input.txt', 'r') as file:
     data = file.readlines()
+
+for index, num in enumerate(data):
+    data[index] = int(num.strip())
+
+
+def checksum(_list, num):
+    for index, i in enumerate(_list):
+        for j in _list[index:]:
+            if i + j == num:
+                return True
+    return False
+
+
+for index, num in enumerate(data):
+    if index <= 24:
+        continue
+    if not checksum(data[index - 25:index], num):
+        print(num)
