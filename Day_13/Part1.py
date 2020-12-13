bab3f2261567863c7b62be20342c249eabebced9
Maxwell <msf9542@g.rit.edu>
2020-12-13 00:14:32 -0500

    finsihed part 1

--- Day_13/Part1.py
+++ Day_13/Part1.py
@@ -1,2 +1,9 @@
-with open("Day_12/input.txt") as file:
+with open("Day_13/input.txt") as file:
     data = [line.strip() for line in file.readlines()]
+
+time = int(data[0])
+_ids = [int(_id) for _id in filter(lambda x: x != 'x', data[1].split(','))]
+delay = [(((time // _id) + 1) * _id) - time for _id in _ids]
+
+print(min(delay), _ids[delay.index(min(delay))],
+      min(delay) * _ids[delay.index(min(delay))])
