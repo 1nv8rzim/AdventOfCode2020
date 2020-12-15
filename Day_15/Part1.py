0c3da1c747d2c032ac5eb5b6ae70c41c6c0d28b1
Maxwell <msf9542@g.rit.edu>
2020-12-15 00:24:58 -0500

    part 1 finished

--- Day_15/Part1.py
+++ Day_15/Part1.py
@@ -1,2 +1,14 @@
 with open("Day_15/input.txt") as file:
     data = [line.strip() for line in file.readlines()]
+
+data = [int(_num) for _num in data[0].split(',')]
+
+while len(data) < 2020:
+    newest = data[-1]
+    if newest not in data[:-1]:
+        data.append(0)
+        continue
+    else:
+        data.append(data[:-1][::-1].index(newest) + 1)
+
+print(data[-1])
