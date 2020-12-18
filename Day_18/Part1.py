f95c5a2329936a77b934523187983b3fbbdd1106
Maxwell <msf9542@g.rit.edu>
2020-12-18 00:32:25 -0500

    finished part1

--- Day_18/Part1.py
+++ Day_18/Part1.py
@@ -1,2 +1,54 @@
-with open("Day_17/input.txt") as file:
+with open("Day_18/input.txt") as file:
     data = [line.strip() for line in file.readlines()]
+
+
+def parse(expr):
+    expr = expr.replace(' ', '')
+
+    def _helper(iter):
+        items = []
+        for item in iter:
+            if item == '(':
+                result, closeparen = _helper(iter)
+                if not closeparen:
+                    raise ValueError(
+                        "bad expression -- unbalanced parentheses")
+                items.append(result)
+            elif item == ')':
+                return items, True
+            else:
+                items.append(item)
+        return items, False
+    return _helper(iter(expr))[0]
+
+
+def math(_line):
+    num1 = _line[0]
+    if isinstance(num1, list):
+        num1 = math(num1)
+
+    operator = _line[1]
+    num2 = _line[2]
+
+    if isinstance(num2, list):
+        num2 = math(num2)
+    num2 = num2
+
+    result = str(eval(num1 + operator + num2))
+    if len(_line) == 3:
+        return result
+
+    _line = [result] + _line[3:]
+    return math(_line)
+
+
+def parse_expression(_line):
+    return math(parse(_line))
+
+
+_sum = 0
+
+for line in data:
+    _sum += int(parse_expression(line))
+
+print(_sum)
