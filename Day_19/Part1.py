262615f2a619ee38e43c54ece1765aa0a2fa97fb
Maxwell <msf9542@g.rit.edu>
2020-12-19 00:51:38 -0500

    finsihed part1

--- Day_19/Part1.py
+++ Day_19/Part1.py
@@ -1,2 +1,43 @@
-with open("Day_18/input.txt") as file:
+import re
+
+with open("Day_19/input.txt") as file:
     data = [line.strip() for line in file.readlines()]
+
+rules = {}
+for line in data[:132]:
+    line = line.split(': ')
+    rules[line[0]] = line[1]
+
+messages = data[133:]
+
+_dict = {}
+
+while '0' not in _dict.keys():
+    for num, rule in rules.items():
+        if rule[0] == '"':
+            _dict[num] = list(rule)[1]
+        else:
+            rule = rule.split()
+            temp = True
+            for _rule in rule:
+                if _rule == '|':
+                    continue
+                if _rule not in _dict.keys():
+                    temp = False
+
+            _result = ''
+            if temp:
+                for _rule in rule:
+                    if _rule == '|':
+                        _result += _rule
+                    else:
+                        _result += _dict[_rule]
+
+                _dict[num] = f'({_result})'
+
+matches = 0
+for messages in messages:
+    if re.fullmatch(_dict['0'], messages):
+        matches += 1
+
+print(matches)
